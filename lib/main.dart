b964f090f0d7d1daf83309047275f18a19ae1529
jac5341931 <inmobiliariaclasificados1@gmail.com>
2022-11-09 17:14:39 -0500

    se modifico login_page

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:touristapp/firebase_options.dart';
 import 'package:touristapp/pages/splash_page.dart';
 
 void main() async {
-  runApp(const MyApp());
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform
   );
+  runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
