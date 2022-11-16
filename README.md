afc4b8fe0bf4ff099fe196961fc73317725347a1
Daniel <104664480+Dagh1981@users.noreply.github.com>
2022-11-16 10:40:33 -0500

    Archivo poidetailsfavorites_page listo
    Código de la interfaz PoiDetailsFavoritesPage listo.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Rama Daniel:
 Para el Sprint 2 contiene el codigo de la interfaz de registro.
 
 Para el Sprint 3 esta rama tiene codigo referente al manejo de excepciones en la interfaz de registro.
+
+Para el Sprint 4 en esta rama se desarrollò codigo para poidetailsfavorites_page.dart a partir de los 
+archivos de los models, poidetails_page.dart y favorites_page.dart
---
+++ lib/pages/location_favorites_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationFavoritesPage extends StatefulWidget {
+  const LocationFavoritesPage({Key? key}) : super(key: key);
+
+  @override
+  State<LocationFavoritesPage> createState() => _LocationFavoritesPageState();
+}
+
+class _LocationFavoritesPageState extends State<LocationFavoritesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
